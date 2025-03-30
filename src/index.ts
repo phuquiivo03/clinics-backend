3a8f2ee0ca5b8cdc2eb6da825db6790cb584ebfe
phuquiivo03 <52100996@student.tdtu.edu.vn>
2025-03-30 14:02:29 +0700

    :rocket: feat: update default page

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,24 @@ app.use(cookieParser(config.cookie.secret));
 app.use("/api", appRouter);
 
 app.get("/", (req, res) => {
-  res.send("Hello, Bun + Express");
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Clinics Management Server</title>
+    </head>
+    <body>
+      <h1>Welcome to the Clinics Management Server</h1>
+      <button 
+        style="background-color: green; color: white; padding: 10px 20px; border: none; cursor: pointer; border-radius: 4px; hover: background-color: darkgreen;" 
+        onclick="window.location.href='/docs'">
+        Go to Docs
+      </button>
+    </body>
+    </html>
+  `);
 });
 
 app.listen(PORT, () => {
