92bc2e1c0a0046f6c62bd944269fa3c7a47a8b8e
Quigiaosu103 <52100996@student.tdtu.edu.vn>
2025-06-30 12:28:37 +0700

    :rocket: feat: update payment api-docs

--- src/swagger.ts
+++ src/swagger.ts
@@ -6,6 +6,7 @@ import { medicalExaminationPaths } from './docs/paths/medical-examination.docs';
 import { prescriptionPaths } from './docs/paths/prescription.docs';
 import { schedulePaths, ScheduleSchemas } from './docs/paths/schedule.swagger';
 import { authPaths } from './docs/paths/auth.docs';
+import { paymentPaths } from './docs/paths/payment.docs';
 
 // Get all route files recursively
 const getRouteFiles = (dir: string): string[] => {
@@ -43,6 +44,7 @@ const options: swaggerJsdoc.Options = {
       ...prescriptionPaths,
       ...schedulePaths,
       ...authPaths,
+      ...paymentPaths,
     },
     components: {
       schemas: {
