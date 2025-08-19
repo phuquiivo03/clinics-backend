05f4581a1bc156e376158817271d74dc114af485
phuquiivo03 <52100996@student.tdtu.edu.vn>
2025-08-20 00:57:35 +0700

    :gear: update return of find schedule by id

--- src/controllers/schedule.controller.ts
+++ src/controllers/schedule.controller.ts
@@ -23,7 +23,7 @@ import {
   type ScheduleService,
   type SchedulePaymentInfo,
 } from '../types/schedules';
-import type { MongooseFindManyOptions } from '../repositories/type';
+import type { MongooseFindManyOptions, MongooseFindOneOptions } from '../repositories/type';
 import type { ConsultationService } from '../types';
 import { config } from '../config';
 import { PaymentMethod, PaymentStatus, type Payment } from '../types/payment';
@@ -262,7 +262,13 @@ const findById: RequestHandler = async (req, res, next) => {
     }
 
     const id = req.params.id as unknown as ObjectId;
-    const schedule = await scheduleService.findById(id);
+    const options: MongooseFindOneOptions = {
+      populateOptions: {
+        path: 'payments.payments',
+        
+      },
+    }
+    const schedule = await scheduleService.findById(id, options);
     if (schedule) {
       return appExpress.response200(schedule);
     }
